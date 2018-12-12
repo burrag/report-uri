dc08ed0f81e50474821f9792678e1601318e0312
Marek Humpolík <marek.humpolik@gmail.com>
2018-12-12 15:08:07 +0100

    added const for key of array in router

--- app/Router/RouterCollectionFactory.php
+++ app/Router/RouterCollectionFactory.php
@@ -5,15 +5,16 @@ namespace App\Router;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
-
 class RouterCollectionFactory
 {
+    private const KEY_REQUEST_HANDLER = 'request-handler';
+
     public function create(): RouteCollection
     {
         $routes = new RouteCollection();
 
-        $routes->add('test', new Route('/test', ['request-handler' => ['testHandler', 'get']]));
-        $routes->add('report', new Route('/report', ['request-handler' => ['reportHandler', 'post']]));
+        $routes->add('test', new Route('/test', [self::KEY_REQUEST_HANDLER => ['testHandler', 'get']]));
+        $routes->add('report', new Route('/report', [self::KEY_REQUEST_HANDLER => ['reportHandler', 'post']]));
 
         return $routes;
     }
