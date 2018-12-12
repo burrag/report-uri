5326995ec1307e0d6b766e495adb6564647752c4
Marek Humpolík <marek.humpolik@gmail.com>
2018-12-12 14:50:35 +0100

    added BUGREPORT_EMAIL to Tracy from env

--- www/index.php
+++ www/index.php
@@ -4,6 +4,6 @@ use Tracy\Debugger;
 
 include __DIR__ . '/../vendor/autoload.php';
 
-Debugger::enable(Debugger::DEVELOPMENT, __DIR__ . '/../log');
+Debugger::enable(Debugger::DETECT, __DIR__ . '/../log', getenv('BUGREPORT_EMAIL') ?: null);
 
 require __DIR__ . '/../app/bootstrap.php';
